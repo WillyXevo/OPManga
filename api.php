6f54aa02fee5116095bc4d82d93e067a511fa23e
WillyXevo <willy.xevo@gmail.com>
2019-03-01 22:05:04 +0700

    fixing api.php hopely final

--- api.php
+++ api.php
@@ -9,19 +9,6 @@
 
 	function list_op($url='')
 	{
-		/*$ch = curl_init();
-		curl_setopt($ch,CURLOPT_URL,$url);
-		curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36");
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-		curl_setopt($ch, CURLOPT_PROXY, null);
-
-		$data = curl_exec($ch);
-		$info = curl_getinfo($ch);
-		$error = curl_error($ch);
-
-		curl_close($ch);*/
-
 		$ch = curl_init();
 		curl_setopt($ch,CURLOPT_URL,$url);
 		curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36");
@@ -31,12 +18,6 @@
 		$data = curl_exec($ch);
 		$info = curl_getinfo($ch);
 		$error = curl_error($ch);
-/*
-		echo "<pre>";
-		print_r($info);
-		print_r($error);
-		echo "</pre>";*/
-
 		curl_close($ch);
 
 		$dom = new simple_html_dom(null, true, true, DEFAULT_TARGET_CHARSET, true, DEFAULT_BR_TEXT, DEFAULT_SPAN_TEXT);
@@ -86,7 +67,6 @@
 
 
 		$html = str_get_html($data);
-		//$html = file_get_html($url);
 		$list_manga = array();
 		$jd = "";
 		foreach($html->find('div#imgholder') as $ell){
@@ -113,53 +93,24 @@
 				array_push($ret, $v);
 			}
 		}
-		//print_r($ret) ;
 		return $ret;
 	}
 
-	//list_op("http://www.mangacanblog.com/baca-komik-one_piece-bahasa-indonesia-online-terbaru.html");
-	
-	//list_op("http://www.komikid.com/manga/one-piece/934/1");
-	//echo '<pre>';
 	if(!isset($_POST['judul'])){
 		$list_manga = list_op("http://www.mangacanblog.com/baca-komik-one_piece-bahasa-indonesia-online-terbaru.html");
-		//print_r($list_manga);
 		echo json_encode($list_manga, JSON_UNESCAPED_SLASHES);
-	}/*else{
-
-		$judul = $_GET['judul'];
-		$link = $_GET['link'];
-
-		$list_manga = ini($link);
-		//_filter_($list_manga);
-		$ret['judul'] = $judul;
-		$ret['img'] = _filter_($list_manga);
-		$ret = array('judul' => $judul, 
-					'img' => _filter_($list_manga)
-						);
-		//print_r($ret);
-		echo json_encode($ret);
-	}*/
-
+	}
 
 	if(isset($_POST['judul'])){
 
 		$judul = $_POST['judul'];
 		$link = $_POST['link'];
 
-		echo $judul;
-		echo $link;
-
 		$list_manga = ini($link);
-		//_filter_($list_manga);
-		/*$ret['judul'] = $judul;
-		$ret['img'] = _filter_($list_manga);*/
 		$ret = array('judul' => $judul, 
 					'img' => _filter_($list_manga)
 						);
-		//print_r($ret);
 		echo json_encode($ret, JSON_UNESCAPED_SLASHES);
 	}
-	///echo '</pre>';
 
 ?>
